f8bdc6bb20e44506c8ad9ac0d4eaafeca7a4fdc0
Jaroslav Kusák <jarekusak@seznam.cz>
2023-09-13 13:47:04 +0200

    třída na zpracování celého záznamu na poslání

---
+++ Tachograph/TaphographRecord.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tachograph
+{
+    class TaphographRecord
+    {
+        // Blok parametry tacho:
+        int wheelDiameter;
+        int carNumber;
+        // Blok parametry vozu:
+        string carType;
+        int gearRatio;
+        int maxWheelDiameter;
+        int maxSpeed; // km/h
+        int kFactor;
+        // Blok počítadla:
+        int totalKilometersDriven;
+        int counter1;
+        int counter2;
+        int counter3;
+        int counter4;
+        int counter5;
+        // Blok módy:
+        int mode0;
+        int mode1;
+        int mode2;
+        int mode3;
+
+        int speedRecordType;
+
+        int recordStep;
+
+        string taphographType;
+        public TaphographRecord() 
+        { 
+        
+        }         
+    }
+}
