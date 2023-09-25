a4cd1243a595fe7be244deed201343daee74cb00
Jaroslav Kusák <jarekusak@seznam.cz>
2023-09-25 10:07:58 +0200

    oddělání zbytečných knihoven

--- Tachograph/WritingInterface.cs
+++ Tachograph/WritingInterface.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
 using System.Net;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls.Primitives;
-using System.Windows.Input;
 
 namespace Tachograph
 {
