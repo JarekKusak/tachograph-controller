7c67243a0029aabdde2cd68dd5eb352bb60a0645
Jaroslav Kusák <jarekusak@seznam.cz>
2023-09-25 13:04:49 +0200

    třída na druhy signálů - přehlednější dekompozice

--- Tachograph/SignalParameters.cs
+++ Tachograph/SignalParameters.cs
@@ -8,6 +8,44 @@ namespace Tachograph
 {
     public class SignalParameters
     {
+        public bool[] ActiveSignals { get; private set; }
+        public bool[] BreakSignals { get; private set; }
+        public bool[] InverseSignals { get; private set; }
+        private bool[] allSignals;
 
+        public SignalParameters(bool[] allSignals) 
+        {
+            this.allSignals = allSignals;
+            ActiveSignals = new bool[allSignals.Length / 3];
+            BreakSignals = new bool[allSignals.Length / 3];
+            InverseSignals = new bool[allSignals.Length / 3];
+        }
+
+        /// <summary>
+        /// Nastaví do proměnných signálů daného typu označené signály
+        /// </summary>
+        public void SetSignalParameters()
+        {
+            int signalIndex = 0;  // Počáteční index v poli allSignals
+
+            // Nastavíme hodnoty v poli ActiveSignals z první třetiny allSignals
+            for (int i = 0; i < ActiveSignals.Length; i++)
+            {
+                ActiveSignals[i] = allSignals[signalIndex];
+                signalIndex++;  // Zvýšíme index v allSignals
+            }
+            // Nastavíme hodnoty v poli BreakSignals ze druhé třetiny allSignals
+            for (int i = 0; i < BreakSignals.Length; i++)
+            {
+                BreakSignals[i] = allSignals[signalIndex];
+                signalIndex++;  // Zvýšíme index v allSignals
+            }
+            // Nastavíme hodnoty v poli InverseSignals z poslední třetiny allSignals
+            for (int i = 0; i < InverseSignals.Length; i++)
+            {
+                InverseSignals[i] = allSignals[signalIndex];
+                signalIndex++;  // Zvýšíme index v allSignals
+            }
+        }
     }
 }
