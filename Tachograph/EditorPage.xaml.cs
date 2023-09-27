e8f1e47a2ba2c2dff76169f814b0c78c463dcc05
Jaroslav Kusák <jarekusak@seznam.cz>
2023-09-27 12:38:47 +0200

    drobné úpravy

--- Tachograph/EditorPage.xaml.cs
+++ Tachograph/EditorPage.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Tachograph
 {
@@ -20,10 +10,14 @@ namespace Tachograph
     /// </summary>
     public partial class EditorPage : Page
     {
+        
         public EditorPage()
         {
             InitializeComponent();
+            
         }
+
+        
 
         private void saveEditorBtn_Click(object sender, RoutedEventArgs e)
         {
